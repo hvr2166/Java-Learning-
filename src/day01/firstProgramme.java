6b82a90041d6202c1c5376a38c07de2f44b0cea2
hvr2166 <rathiharsh841@gmail.com>
2025-09-26 22:21:20 +0530

    first programme

---
+++ src/day01/firstProgramme.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class firstProgramme {
+
+    public static void main (String[] args){
+
+        //first java programme
+
+        System.out.print("hello world!");
+    }
+}
